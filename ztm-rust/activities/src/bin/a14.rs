a9eb1fb1f2dfdc6b2282ad401f6e5231bb5616ae
Rodrez <fabian.rodrez@gmail.com>
2022-02-08 20:10:51 -0500

    Completed Activity a14

--- ztm-rust/activities/src/bin/a14.rs
+++ ztm-rust/activities/src/bin/a14.rs
@@ -11,4 +11,47 @@
 // * Use an if expression to determine which person's info should be printed
 // * The name and colors should be printed using a function
 
-fn main() {}
+
+// * Use a struct for a persons age, name, and favorite color
+// * The color and name should be stored as a String
+struct Person {
+    age: i32,
+    name: String,
+    color: String
+}
+
+fn print_info(name: &str, color: &str ){
+    println!("Name: {:?},  favorite color: {:?}", name, color)
+}
+
+
+
+fn main() {
+// * Create and store at least 3 people in a vector
+    let people = vec![
+        Person {
+            age: 5,
+            name: "Peter".to_owned(),
+            color: String::from("blue")
+        },
+        Person {
+            age: 11,
+            name: "Mally".to_owned(),
+            color: String::from("red")
+        },
+        Person {
+            age: 9,
+            name: "Dan".to_owned(),
+            color: String::from("purple")
+        }
+    ];
+    // * Iterate through the vector using a for..in loop
+    for person in people {
+        // * Use an if expression to determine which person's info should be printed
+        if person.age < 10{
+            print_info(&person.name, &person.color);
+
+        }
+    }
+
+}
