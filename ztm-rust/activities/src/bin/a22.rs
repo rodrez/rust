40688c2a59152e0e4a1c16fc5db226de32552be3
Rodrez <fabian.rodrez@gmail.com>
2022-03-09 23:34:00 -0500

    Completed a26 external modules using chronos

--- ztm-rust/activities/src/bin/a22.rs
+++ ztm-rust/activities/src/bin/a22.rs
@@ -23,12 +23,45 @@ fn clamp(n: i32, lower: i32, upper: i32) -> i32 {
 
 /// Divides a and b.
 fn div(a: i32, b: i32) -> Option<i32> {
-    Some(a / b)
+    if b == 0 {
+        None
+    } else {
+        Some(a / b)
+    }
 }
 
 /// Takes two strings and places them immediately one after another.
 fn concat(first: &str, second: &str) -> String {
-    format!("{} {}", first, second)
+    format!("{}{}", first, second)
 }
 
 fn main() {}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn check_clamp() {
+        let cl1 = clamp(10, 100, 100);
+        let cl2 = 100;
+
+        assert_eq!(cl1, cl2, "should return 100")
+    }
+
+    #[test]
+    fn check_div() {
+        let d1 = div(1, 1);
+        let d2 = Some(1);
+
+        assert_eq!(d1, d2, "wrong division")
+    }
+
+    #[test]
+    fn check_concat() {
+        let first = concat("hello", "world");
+        let sec = "helloworld";
+
+        assert_eq!(first, sec, "string was not placed immediately.")
+    }
+}
--- ztm-rust/activities/src/bin/a26.rs
+++ ztm-rust/activities/src/bin/a26.rs
@@ -7,5 +7,10 @@
 // * Use the `chrono` crate to work with time
 // * (OPTIONAL) Read the documentation section `Formatting and Parsing`
 //   for examples on how to create custom time formats
+use chrono::Utc;
 
-fn main() {}
+fn main() {
+    let local = Utc::now();
+    let formatted_date = local.format("%v").to_string();
+    println!("{:?}", formatted_date);
+}
