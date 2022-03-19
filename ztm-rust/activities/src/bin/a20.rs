391789e8377c73bf5f783bd104fb23e9f636a2ce
Rodrez <fabian.rodrez@gmail.com>
2022-03-18 22:13:00 -0400

    Pushing activity 20

--- ztm-rust/activities/src/bin/a20.rs
+++ ztm-rust/activities/src/bin/a20.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 // Topic: User input
 //
 // Requirements:
@@ -23,6 +25,7 @@
 // * The program should be case-insensitive (the user should be able to type
 //   Reboot, reboot, REBOOT, etc.)
 
+#[derive(Debug)]
 enum States {
     Off,
     Sleep,
@@ -31,10 +34,44 @@ enum States {
     Hibernate,
 }
 
-fn print_msg(state: &States){
-    
+impl States {
+    fn from_str(s: &str) -> Option<States> {
+        match s.trim().to_lowercase().as_str() {
+            "off" => Some(States::Off),
+            "sleep" => Some(States::Sleep),
+            "reboot" => Some(States::Reboot),
+            "shutdown" => Some(States::Shutdown),
+            "hibernate" => Some(States::Hibernate),
+            _ => None,
+        }
+    }
+}
+
+fn print_msg(state: &States) {
+    // * Use a function with a match expression to print out the power messages
+    // * The function should accept the enum as an input
+
+    use States::*;
+    match state {
+        Off => println!("Powering off"),
+        Sleep => println!("Going to sleep"),
+        Reboot => println!("Rebooting"),
+        Shutdown => println!("Shutting down"),
+        Hibernate => println!("Hibernating"),
+    }
 }
 
 fn main() {
-    
+    // * Use a match expression to convert the user input into the power state enum
+    let mut buffer = String::new();
+    let user_input = io::stdin().read_line(&mut buffer);
+
+    if user_input.is_ok() {
+        match States::from_str(&buffer) {
+            Some(state) => print_msg(&state),
+            None => println!("Invalid entry"),
+        }
+    } else {
+        println!("Error reading input")
+    }
 }
